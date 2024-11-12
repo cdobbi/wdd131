b8659ae0db23a933587f62164f1ad37d907fbff5
cdobbi <dob24001@byui.edu>
2024-11-12 12:41:36 -0700

    Week02 - activites progress

---
+++ week02/scripts/bom.js
@@ -0,0 +1,13 @@
+document.getElementById("lastModified").textContent = document.lastModified;
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector(".list");
+const li = document.createElement("li");
+const deleteButton = document.createElement("button");
+li.textContent = input.value;
+deleteButton.textContent = "❌";
+li.append(deleteButton);
+list.append(li);
