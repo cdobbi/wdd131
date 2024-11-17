0641350b04adad57730425da9ed84439d5c36b2e
cdobbi <dob24001@byui.edu>
2024-11-16 21:09:10 -0700

    making progress

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,9 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
 
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
 });
 
 document.getElementById("year").textContent = new Date().getFullYear();
