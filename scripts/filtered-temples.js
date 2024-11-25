0588ccc959ef52cb7a3d8ecd368056729cb77167
cdobbi <dob24001@byui.edu>
2024-11-25 15:31:21 -0700

    set up files for filtered temples

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,20 @@
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = document.lastModified;
+
+/* I can't get this to commit to github. */
+/*JavaScript
+Remember that all of your JavaScript references in this class should be to external files.
+Support the footer copyright year and date last modified output through a valid JavaScript reference. You already have one or could use the temples.js file.
+Apply a responsive hamburger effect to your existing navigation menu using JavaScript.
+The hamburger button should only show in the mobile view.
+Clicking the hamburger button toggles the navigation menu items from viewable to not viewable.
+Use a symbol, such as an "X" to close the hamburger menu.*/
