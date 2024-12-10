5bbbe35526c8a6d9ae44ce148266f8fd379cc939
cdobbi <dob24001@byui.edu>
2024-12-10 14:09:16 -0700

    ...

--- project/scripts/rabbitry.js
+++ project/scripts/rabbitry.js
@@ -1,9 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+    // Hamburger menu functionality
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const navMenu = document.getElementById('nav-menu').querySelector('ul');
+
+    hamburgerMenu.addEventListener('click', function() {
+        navMenu.classList.toggle('show');
+    });
+
+    // Trivia functionality
     const trivia = [
         "Hopping Trivia: The Rhinelander has a distinctive butterfly marking on its nose.",
-        "Hopping Trivia: Rhinelanders are rare with about 2,000 in the world.",
         "Hopping Trivia: The Rhinelander is a rare breed of rabbit.",
-        "Hopping Trivia: Rhinelanders are medium to large-sized rabbits, weighing between 6.5 to 10 pounds",
+        "Hopping Trivia: Rhinelanders weigh between 6.5 to 10 pounds",
         "Hopping Trivia: Rhinelanders are intelligent and trainable.",
         "Hopping Trivia: In order to show a Rhinelander it must have at least 3 spots on the thigh.",
         "Hopping Trivia: The Rhinelander was first bred in the 1920s.",
@@ -11,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
         "Hopping Trivia: Rhinelander rabbits require minimal grooming as they only shed twice a year.",
         "Hopping Trivia: They originated in North Rhine-Westphalia, Germany, in the early 1900s.",
         "Hopping Trivia: Rabbit ears move independently by 180 degrees, enabling them to hear from two directions at once.",
-        "Hopping Trivia: A rabbit’s sense of smell is even better than their eyesight.",
+        "Hopping Trivia: A rabbits sense of smell is even better than their eyesight.",
         "Hopping Trivia: Rabbits can run up to 18 miles per hour.",
         "Hopping Trivia: Rabbit tails are used to distract predators during a chase and to signal danger to other rabbits.",
         "Hopping Trivia: Rabbits have nearly 360° panoramic vision, allowing them to detect predators from almost any direction.",
@@ -32,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
         "Hopping Trivia: Rhinelanders are known for their friendly and outgoing personalities.",
         "Hopping Trivia: Rhinelanders are known for their playful and curious nature.",
         "Hopping Trivia: Temperatures above 85 degrees Fahrenheit sterilize bucks.",
-        "Hopping Trivia: Rhinelanders have a stripe of color on that extends the length of their back.",
+        "Hopping Trivia: Rhinelanders have a stripe that extends the length of their back.",
         "Hopping Trivia: The Rhinelander is known for their distinctive coat markings.",
         "Hopping Trivia: A rabbit doe has 2 uteruses",
         "Hopping Trivia: Rabbits have 2 sets of incisors",
@@ -46,12 +54,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const randomTrivia = trivia[Math.floor(Math.random() * trivia.length)];
     console.log("Hopping Trivia: ", randomTrivia);
     document.getElementById("random-trivia").textContent = randomTrivia;
-
-    // Hamburger menu functionality
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const navMenu = document.getElementById('nav-menu').querySelector('ul');
-
-    hamburgerMenu.addEventListener('click', function() {
-        navMenu.classList.toggle('show');
-    });
+    document.getElementById("lastModified").textContent = document.lastModified;
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+    console.log("DOM fully loaded and parsed");
 });
