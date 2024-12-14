f48896e93f5e0fe24ab1133ad863e5b1d475dd76
cdobbi <dob24001@byui.edu>
2024-12-14 09:14:12 -0700

    ...

--- project/scripts/rabbitry.js
+++ project/scripts/rabbitry.js
@@ -25,67 +25,67 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Hamburger Menu Toggle
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const navMenu = document.querySelector('nav ul');
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const navMenu = document.querySelector('nav ul');
 
-    console.log('Hamburger Menu:', hamburgerMenu);
-    console.log('Nav Menu:', navMenu);
+console.log('Hamburger Menu:', hamburgerMenu);
+console.log('Nav Menu:', navMenu);
 
-    if (hamburgerMenu && navMenu) {
-        hamburgerMenu.addEventListener('click', function () {
-            navMenu.classList.toggle('show');
-            hamburgerMenu.classList.toggle('active');
-            const isActive = hamburgerMenu.classList.contains('active');
-            hamburgerMenu.setAttribute('aria-expanded', `${isActive}`);
-            console.log('Hamburger menu clicked. Classes toggled.');
-        });
-    } else {
-        console.error('Hamburger menu or nav menu not found');
-    }
+if (hamburgerMenu && navMenu) {
+    hamburgerMenu.addEventListener('click', function () {
+        navMenu.classList.toggle('show');
+        hamburgerMenu.classList.toggle('active');
+        const isActive = hamburgerMenu.classList.contains('active');
+        hamburgerMenu.setAttribute('aria-expanded', `${isActive}`);
+        console.log('Hamburger menu clicked. Classes toggled.');
+    });
+} else {
+    console.error('Hamburger menu or nav menu not found');
+}
 
     // Display Random Trivia
-    const triviaArray = [
-        'Hopping Trivia: The Rhinelander has a distinctive butterfly marking on its nose.',
-        'Hopping Trivia: The Rhinelander is a rare breed of rabbit.',
-        'Hopping Trivia: Rhinelanders weigh between 6.5 to 10 pounds.',
-        'Hopping Trivia: Rhinelanders are intelligent and trainable.',
-        'Hopping Trivia: The Rhinelanders originated in Germany.',
-        'Hopping Trivia: Rabbit ears move independently by 180 degrees, enabling them to hear from two directions at once.',
-        'Hopping Trivia: A rabbit’s sense of smell is even better than their eyesight.',
-        'Hopping Trivia: Rabbits can run up to 18 miles per hour.',
-        'Hopping Trivia: Rabbit tails are used to distract predators during a chase and to signal danger to other rabbits.',
-        'Hopping Trivia: Rabbits have nearly 360° panoramic vision, allowing them to detect predators from almost any direction.',
-        'Hopping Trivia: Rhinelanders were created by crossing a Japanese Harlequin and a Checkered Giant.',
-        'Hopping Trivia: Rabbits are herbivores.',
-        'Hopping Trivia: Rabbits are crepuscular, meaning they are most active at dawn and dusk.',
-        'Hopping Trivia: Rabbits are social animals.',
-        'Hopping Trivia: Rabbits communicate through body language.',
-        'Hopping Trivia: Rabbits will give you kisses to show affection.',
-        'Hopping Trivia: Rabbits are prey animals.',
-        'Hopping Trivia: Rabbits have a third eyelid called a nictitating membrane.',
-        'Hopping Trivia: Rabbits love to eat dandelion leaves, roses and flowers.',
-        'Hopping Trivia: Rhinelanders are known for their friendly and outgoing personalities.',
-        'Hopping Trivia: Rhinelanders are known for their playful and curious nature.',
-        'Hopping Trivia: Temperatures above 85 degrees Fahrenheit sterilize bucks.',
-        'Hopping Trivia: The Rhinelander is known for their distinctive coat markings.',
-        'Hopping Trivia: A rabbit doe has 2 uteruses.',
-        'Hopping Trivia: Rabbits have 2 sets of incisors.',
-        'Hopping Trivia: Rabbits have 28 teeth.',
-        'Hopping Trivia: Rabbits have a lifespan of 1.6-12 years.',
-        'Hopping Trivia: Rhinelanders require daily interaction and mental stimulation.',
-    ];
+const triviaArray = [
+    'Hopping Trivia: The Rhinelander has a distinctive butterfly marking on its nose.',
+    'Hopping Trivia: The Rhinelander is a rare breed of rabbit.',
+    'Hopping Trivia: Rhinelanders weigh between 6.5 to 10 pounds.',
+    'Hopping Trivia: Rhinelanders are intelligent and trainable.',
+    'Hopping Trivia: The Rhinelanders originated in Germany.',
+    'Hopping Trivia: Rabbit ears move independently by 180 degrees, enabling them to hear from two directions at once.',
+    'Hopping Trivia: A rabbit’s sense of smell is even better than their eyesight.',
+    'Hopping Trivia: Rabbits can run up to 18 miles per hour.',
+    'Hopping Trivia: Rabbit tails are used to distract predators during a chase and to signal danger to other rabbits.',
+    'Hopping Trivia: Rabbits have nearly 360° panoramic vision, allowing them to detect predators from almost any direction.',
+    'Hopping Trivia: Rhinelanders were created by crossing a Japanese Harlequin and a Checkered Giant.',
+    'Hopping Trivia: Rabbits are herbivores.',
+    'Hopping Trivia: Rabbits are crepuscular, meaning they are most active at dawn and dusk.',
+    'Hopping Trivia: Rabbits are social animals.',
+    'Hopping Trivia: Rabbits communicate through body language.',
+    'Hopping Trivia: Rabbits will give you kisses to show affection.',
+    'Hopping Trivia: Rabbits are prey animals.',
+    'Hopping Trivia: Rabbits have a third eyelid called a nictitating membrane.',
+    'Hopping Trivia: Rabbits love to eat dandelion leaves, roses and flowers.',
+    'Hopping Trivia: Rhinelanders are known for their friendly and outgoing personalities.',
+    'Hopping Trivia: Rhinelanders are known for their playful and curious nature.',
+    'Hopping Trivia: Temperatures above 85 degrees Fahrenheit sterilize bucks.',
+    'Hopping Trivia: The Rhinelander is known for their distinctive coat markings.',
+    'Hopping Trivia: A rabbit doe has 2 uteruses.',
+    'Hopping Trivia: Rabbits have 2 sets of incisors.',
+    'Hopping Trivia: Rabbits have 28 teeth.',
+    'Hopping Trivia: Rabbits have a lifespan of 1.6-12 years.',
+    'Hopping Trivia: Rhinelanders require daily interaction and mental stimulation.',
+];
 
-    function displayRandomTrivia() {
-        const randomTrivia = triviaArray[Math.floor(Math.random() * triviaArray.length)];
-        const triviaElement = document.getElementById('random-trivia');
-        if (triviaElement) {
-            triviaElement.textContent = randomTrivia;
-        } else {
-            console.error('Element with ID "random-trivia" not found');
-        }
+function displayRandomTrivia() {
+    const randomTrivia = triviaArray[Math.floor(Math.random() * triviaArray.length)];
+    const triviaElement = document.getElementById('random-trivia');
+    if (triviaElement) {
+        triviaElement.textContent = randomTrivia;
+    } else {
+        console.error('Element with ID "random-trivia" not found');
     }
+}
 
-    displayRandomTrivia(); // Call the function once to display trivia
+displayRandomTrivia(); // Call the function once to display trivia
 
     // Handle Form Submission on 'contact-us.html'
     if (window.location.pathname.includes('contact-us.html')) {
